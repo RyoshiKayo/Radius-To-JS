9eff0c4b846c99723d3c3cb428a45d35b4b2479a
RyoshiKayo <caymon@ryoshicayo.net>
2017-02-28 22:31:26 -0800

    update

--- radius-to-js.js
+++ radius-to-js.js
@@ -1,5 +1,6 @@
 //This script takes a Radius log and converts it into js format
 let fs = require('fs')
+let lineReader = require('line-reader')
 
 global.RADIUS_LOG = __dirname + process.argv[2] //the third argument is the first user input
 
@@ -10,11 +11,30 @@ if (!fs.existsSync(OUTPUT_PATH)){
 }
 
 //Setup our file.
-let OUTPUT_FILE = OUTPUT_PATH + RADIUS_LOG + `_out`
-let fileBase = "module.exports = {\n}"
+let OUTPUT_FILE = OUTPUT_PATH + RADIUS_LOG + `_out.js`
+let fileBase = "module.exports = {\n"
 fs.writeFile(OUTPUT_FILE, fileBase, function(err) {
     if (err) {
         return console.log(err)
     }
 })
 
+function addLine(line){
+    let finalLine = ""
+
+    //Split up the line up by spaces.
+    lineArray = line.split(/(\s+)/)
+    for (let i = 0, len = lineArray.length; i < len; i++){
+        if (lineArray[i].startsWith("\t")){
+
+        } else {
+            //If the beginning of the line does not have a tab, then it is the date timestamp
+            finalLine = line.split(' ').join('-')
+        }
+    }
+    fs.writeFile(OUTPUT_FILE, finalLine, function(err){
+        if (err) {
+            console.log(err)
+        }
+    })
+}
