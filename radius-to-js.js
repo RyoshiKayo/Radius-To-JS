cb04e3a772215f4e263fc26676073a77ded25194
RyoshiKayo <caymon@ryoshicayo.net>
2017-03-03 22:09:29 -0800

    Finally working for logs from FreeRadius v2.2.0

--- radius-to-js.js
+++ radius-to-js.js
@@ -1,69 +1,50 @@
 //This script takes a Radius log and converts it into js format
 let fs = require('fs');
-let path = require('path');
+let path = require('path'); 
 let lineReader = require('line-reader');
-let tmp = "";
 
-if (!process.argv[2]) return console.log('No arg');
-let RADIUS_LOG = {
-    filename: (fs.lstatSync(process.argv[2]).isFile ? process.argv[2] : path.basename(process.argv[2])),   
-    fileFullPath: (fs.lstatSync(process.argv[2]).isDirectory ? process.argv[2] : path.join(__dirname, process.argv[2]))
+let toWrite = "";
+let radiusLog = process.argv[2]; //Assume that the arg is going to be the full path.
+let outputFile = path.join(__dirname, path.basename(radiusLog) + "_output.js");
+
+console.log('> Starting...');
+
+//Make sure that the arg contains the filename, that it isn't a js file, and the output isn't already there.
+if (radiusLog.slice(-1) == path.sep) {
+    return console.log('Argument must have the filename!');
+} else if (path.extname(radiusLog) == '.js') {
+    return console.log('File given is js file!');
+} else if (fs.exists(outputFile)) {
+    return console.log('A js version already exists!');
 }
 
-console.log('>Starting...');
+//Add the beginning of out file.
+toWrite += "modules.export { \n";
 
-//Create out output folder.
-let OUTPUT_PATH = path.join(__dirname, 'out');
-if (!fs.existsSync(OUTPUT_PATH)){
-    fs.mkdirSync(OUTPUT_PATH);
-    console.log('>Created output path...');
-}
-
-//Setup our file.
-let OUTPUT_FILE = path.join(OUTPUT_PATH, RADIUS_LOG.fileName + `_out.js`);
-tmp += `module.exports = {${"\n"}`;
-console.log('>File init finished...');
-
-lineReader.eachLine(RADIUS_LOG.fileFullPath, function(line){
-    if (line = null || line == "") return;
-    let lineArray2 = [];
-    let lineArray = line.toString().split(' ').filter(val => val !== '=');
-    
-    //Check if the array has an equals sign for any values.
-    for (i = 0, len = lineArray.length; i < len; i++) {
-        //Split up the value again if contains and equals.
-        if (lineArray[i].includes('=') && i !== 0){
-            lineArray2 = lineArray[i].split(' ').filter(val => val !== '=');
-            lineArray.splice(i, i);
-        }
-
-        lineArray[i].replace("\"", "\'");
-        if (i % 2 == 0) {
-            lineArray[i] = `'${lineArray[i]}'`;
+lineReader.eachLine(radiusLog, function(line, last) {
+    if (line.startsWith("\t") && line !== "") {
+        let lineArray = line.toString().split(' ').filter(val => val !== '=');
+        if (lineArray[0] !== "\tEvent-Timestamp") {
+            toWrite += "\t" + lineArray[0] + ": \'" + lineArray[1].replace(/['"]+/g, '') + "\',\n";
         } else {
-            lineArray[i].replace("\"", "");
+            let tempArray = [...lineArray];
+            tempArray.splice(0, 1);
+            toWrite += "\t" + lineArray[0] + ": \'" + tempArray.join('-').replace(/['"]+/g, '') + "\',\n";
         }
+    } else if (!line.startsWith("\t") && line !== "") {
+        toWrite += "\t" + line.replace(/ /g, '-') + ": {\n";
+    } else if (line == "") {
+        toWrite += "\t},\n";
+    } else {
+        console.log('Got a wierd line...');
     }
 
-    //Check if there is a lineArray2.
-    if (lineArray.length == 1) {
-        if (lineArray2.length >=1 && lineArray2[1] != null && !lineArra2[1].includes('\"')){
-            lineArray2[1].replace("\"", "\'");
-        }
-        tmp += `${"\t"}${lineArray[0]}: {${"\n"}`;
-        tmp += `${"\t"}${"\tab"}${lineArray2[0]}: '${lineArray2[1]}'${"\n"}`;
-        tmp += `${"\t}"}${"\n"}`;
+    if (last) {
+        toWrite += "}";
+        fs.writeFile(outputFile, toWrite, function(err){
+            if (err) console.log(err);
+        });
+        console.log('> Writing file...');
+        return console.log('> Finished!');
     }
-
-    if (!lineArray[1])
-    tmp += `${"\t"}${lineArray[0]}:  ${lineArray[1]},${"\n"}`;
-}).then(function(err) {
-    if (err) throw err;
-
-    tmp += `${"\t"}}${"\n"}}`;
-    fs.writeFileSync(OUTPUT_FILE, tmp, function(err){
-        if (err) console.log(err);
-    });
-});
-
-console.log('>Done!');
+}); 
